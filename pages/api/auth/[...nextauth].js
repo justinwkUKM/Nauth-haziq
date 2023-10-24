ea9374f4093aecb81267486604ae75f072389f0c
Waqas Khalid Obeidy <waqasobeidy@gmail.com>
2023-10-24 13:57:21 +0800

    added azure ad

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import AzureADProvider from "next-auth/providers/azure-ad";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -7,6 +8,11 @@ export const authOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    AzureADProvider({
+      clientId: process.env.AZURE_AD_CLIENT_ID,
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+      tenantId: process.env.AZURE_AD_TENANT_ID,
     }),
   ],
   secret:process.env.NEXT_AUTH_SECRET,
